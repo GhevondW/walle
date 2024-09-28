b3b2c92e743cf05ce2dbb2d73055aea275a8d6d6
Ghevond <ghevond.98@gmail.com>
2024-09-28 21:46:58 +0400

    pimpl

--- include/walle/core/fast_pimpl.hpp
+++ include/walle/core/fast_pimpl.hpp
@@ -8,85 +8,69 @@
 namespace walle::core {
 
 template <typename T, std::size_t Size, std::size_t Align>
-class fast_pimpl {
+class fast_pimpl final {
 public:
     template <typename... Args>
-    explicit fast_pimpl(Args&&... args);
+    explicit fast_pimpl(Args&&... args) noexcept(noexcept(T(std::declval<Args>()...)))
+        : _storage() {
+        new (as_type()) T(std::forward<Args>(args)...);
+    }
 
-    fast_pimpl(const fast_pimpl& other);
-    fast_pimpl(fast_pimpl&& other) noexcept;
+    fast_pimpl(fast_pimpl& other) noexcept(noexcept(T(std::declval<const T&>())))
+        : fast_pimpl(static_cast<const T&>(other.data_ref())) {}
 
-    fast_pimpl& operator=(const fast_pimpl& other);
-    fast_pimpl& operator=(fast_pimpl&& other) noexcept;
+    fast_pimpl(const fast_pimpl& other) noexcept(noexcept(T(std::declval<const T&>())))
+        : fast_pimpl(other.data_ref()) {}
 
-    ~fast_pimpl() noexcept;
+    fast_pimpl(fast_pimpl&& other) noexcept(noexcept(T(std::declval<T>())))
+        : fast_pimpl(std::move(other.data_ref())) {}
+
+    fast_pimpl& operator=(const fast_pimpl& other) noexcept(noexcept(std::declval<T&>() = std::declval<const T&>())) {
+        if (this == std::addressof(other)) {
+            return *this;
+        }
+
+        data_ref() = other.data_ref();
+        return *this;
+    }
+
+    fast_pimpl& operator=(fast_pimpl&& other) noexcept(noexcept(std::declval<T&>() = std::declval<T>())) {
+        if (this == std::addressof(other)) {
+            return *this;
+        }
+
+        data_ref() = std::move(other.data_ref());
+        return *this;
+    }
+
+    ~fast_pimpl() noexcept {
+        static_assert(noexcept(std::declval<T*>()->~T()));
+        static_assert(Size != 0);
+        static_assert(sizeof(T) == Size);
+        static_assert(alignof(T) == Align);
+
+        std::destroy_at(as_type());
+    }
 
     const T& data_ref() const noexcept {
-        return *data();
+        return *as_type();
     }
 
     T& data_ref() noexcept {
-        return *data();
+        return *as_type();
     }
 
-    T* data() noexcept {
+private:
+    T* as_type() noexcept {
         return std::launder(reinterpret_cast<T*>(&_storage));
     }
 
-    const T* data() const noexcept {
+    const T* as_type() const noexcept {
         return std::launder(reinterpret_cast<const T*>(&_storage));
     }
 
 private:
     alignas(Align) std::byte _storage[Size];
 };
-
-template <typename T, std::size_t Size, std::size_t Align>
-template <typename... Args>
-fast_pimpl<T, Size, Align>::fast_pimpl(Args&&... args)
-    : _storage() {
-    new (data()) T(std::forward<Args>(args)...);
-}
-
-template <typename T, std::size_t Size, std::size_t Align>
-fast_pimpl<T, Size, Align>::fast_pimpl(const fast_pimpl& other)
-    : _storage() {
-    new (data()) T(other.data_ref());
-}
-
-template <typename T, std::size_t Size, std::size_t Align>
-fast_pimpl<T, Size, Align>::fast_pimpl(fast_pimpl&& other) noexcept
-    : _storage() {
-    new (data()) T(std::move(other.data_ref()));
-}
-
-template <typename T, std::size_t Size, std::size_t Align>
-fast_pimpl<T, Size, Align>& fast_pimpl<T, Size, Align>::operator=(const fast_pimpl& other) {
-    if (std::addressof(other) == this) {
-        return *this;
-    }
-
-    data_ref() = other.data_ref();
-    return *this;
-}
-
-template <typename T, std::size_t Size, std::size_t Align>
-fast_pimpl<T, Size, Align>& fast_pimpl<T, Size, Align>::operator=(fast_pimpl&& other) noexcept {
-    if (std::addressof(other) == this) {
-        return *this;
-    }
-
-    data_ref() = std::move(other.data_ref());
-    return *this;
-}
-
-template <typename T, std::size_t Size, std::size_t Align>
-fast_pimpl<T, Size, Align>::~fast_pimpl() noexcept {
-    static_assert(Size != 0);
-    static_assert(sizeof(T) == Size);
-    static_assert(alignof(T) == Align);
-
-    std::destroy_at(data());
-}
 
 } // namespace walle::core
--- test/test_core_fast_pimpl.cpp
+++ test/test_core_fast_pimpl.cpp
@@ -7,7 +7,109 @@
 
 using namespace std::chrono_literals;
 
-TEST_CASE("walle::core::fast_pimpl") {
+TEST_CASE("walle::core::fast_pimpl.just_works") {
+    int counter = 0;
+    struct sample {
+        sample(int* cnt)
+            : counter(cnt) {
+            add_one();
+        }
+
+        sample(const sample& other)
+            : counter(other.counter) {
+            add_one();
+        }
+
+        sample(sample&& other) noexcept
+            : counter(other.counter) {
+            add_one();
+        }
+
+        sample& operator=(const sample& other) {
+            counter = other.counter;
+            add_one();
+            return *this;
+        }
+
+        sample& operator=(sample&& other) noexcept {
+            counter = other.counter;
+            add_one();
+            return *this;
+        }
+
+        ~sample() {
+            add_one();
+        }
+
+        void add_one() {
+            *counter = *counter + 1;
+        }
+
+        int* counter = nullptr;
+    };
+
+    {
+        REQUIRE(counter == 0);
+        walle::core::fast_pimpl<sample, sizeof(sample), alignof(sample)> impl(&counter);
+        REQUIRE(counter == 1);
+    }
+    REQUIRE(counter == 2);
+
+    {
+        walle::core::fast_pimpl<sample, sizeof(sample), alignof(sample)> impl1(&counter);
+        REQUIRE(counter == 3);
+        walle::core::fast_pimpl<sample, sizeof(sample), alignof(sample)> impl2(&counter);
+        REQUIRE(counter == 4);
+
+        impl1 = impl2;
+        REQUIRE(counter == 5);
+    }
+    REQUIRE(counter == 7);
+
+    {
+        walle::core::fast_pimpl<sample, sizeof(sample), alignof(sample)> impl1(&counter);
+        REQUIRE(counter == 8);
+        walle::core::fast_pimpl<sample, sizeof(sample), alignof(sample)> impl2(std::move(impl1));
+        REQUIRE(counter == 9);
+    }
+    REQUIRE(counter == 11);
+
+    {
+        walle::core::fast_pimpl<sample, sizeof(sample), alignof(sample)> impl1(&counter);
+        REQUIRE(counter == 12);
+        walle::core::fast_pimpl<sample, sizeof(sample), alignof(sample)> impl2(
+            static_cast<const walle::core::fast_pimpl<sample, sizeof(sample), alignof(sample)>&>(impl1));
+
+        REQUIRE(counter == 13);
+    }
+    REQUIRE(counter == 15);
+
+    {
+        walle::core::fast_pimpl<sample, sizeof(sample), alignof(sample)> impl1(&counter);
+        REQUIRE(counter == 16);
+        walle::core::fast_pimpl<sample, sizeof(sample), alignof(sample)> impl2(impl1);
+        REQUIRE(counter == 17);
+    }
+    REQUIRE(counter == 19);
+
+    {
+        walle::core::fast_pimpl<sample, sizeof(sample), alignof(sample)> impl1(&counter);
+        REQUIRE(counter == 20);
+        walle::core::fast_pimpl<sample, sizeof(sample), alignof(sample)> impl2 = impl1;
+        REQUIRE(counter == 21);
+    }
+    REQUIRE(counter == 23);
+
+    {
+        walle::core::fast_pimpl<sample, sizeof(sample), alignof(sample)> impl1(&counter);
+        REQUIRE(counter == 24);
+        walle::core::fast_pimpl<sample, sizeof(sample), alignof(sample)> impl2 = std::move(impl1);
+        REQUIRE(counter == 25);
+    }
+    REQUIRE(counter == 27);
+}
+
+TEST_CASE("walle::core::fast_pimpl.sample") {
     test::common::sample_pimpl pimpl("foo");
     REQUIRE(std::string(pimpl.foo()) == "foo");
 }
